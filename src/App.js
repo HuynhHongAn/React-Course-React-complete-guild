b3989c5460f6deab292072119f238c7cbd636b17
1512014 <1512014@student.hcmus.edu.vn>
2019-06-20 15:23:46 +0700

    Using properties in Conponent (state)

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Andrew", age: "22"},
+      {name: "Bob", age: "25"},
+      {name: "Charlos", age: "32"}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I am react app</h1>
-        <Person></Person>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hello world!</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
       </div>
     );
     //return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Hi I\'m Andrew'));
