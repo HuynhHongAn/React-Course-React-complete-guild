ae13867215a8393ee2e080331efe7dce7937c81e
1512014 <1512014@student.hcmus.edu.vn>
2019-06-24 00:57:29 +0700

    Refs for Class

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,6 +5,15 @@ import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount(){
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
     render() {
         console.log('[Person.js] is rendering...')
         return (
@@ -13,6 +22,8 @@ class Person extends Component {
             <Aux>
                 <input
                     type="text" 
+                    // ref = {(inputEl) => {this.inputElement = inputEl}}
+                    ref = {this.inputElementRef}
                     onChange={this.props.changed} 
                     value={this.props.name}>
                 </input>
